6a85017d6146186bba93fd13112c962a45c44d09
Hector <hector.morales.carnice@gmail.com>
2023-08-26 03:13:16 +0200

    chore(deps): merge dependabot branches

--- src/ClickOutside.ts
+++ src/ClickOutside.ts
@@ -1,17 +1,17 @@
 import { DirectiveBinding, ObjectDirective } from "vue";
 
 type FocusableElement = HTMLInputElement | HTMLTextAreaElement;
-type GenericEventHandler = (event: any) => void;
+type MouseEventHandler = (event: MouseEvent) => void;
 
 interface ExtendedDirective extends ObjectDirective {
-  handleClickOutside: GenericEventHandler;
+  handleClickOutside: MouseEventHandler;
 }
 
 const clickOutside = {
   beforeMount: (el: FocusableElement, binding: DirectiveBinding) => {
     const thisDirective = binding.dir as ExtendedDirective;
-    thisDirective.handleClickOutside = (event: any) => {
-      if (!(el == event.target || el.contains(event.target))) {
+    thisDirective.handleClickOutside = (event: MouseEvent) => {
+      if (!(el == event.target || el.contains(event.target as Node | null))) {
         binding.value();
       }
     };
